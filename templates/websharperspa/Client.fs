f9eac391244da02f5c9ab4469e582ddf09c4f049
Ádám Abonyi-Tóth <atadi96@gmail.com>
2017-07-13 17:03:12 +0200

    Updated WebSharper Single Page Application template to Zafir

--- templates/websharperspa/Client.fs
+++ templates/websharperspa/Client.fs
@@ -5,10 +5,13 @@ open WebSharper.JavaScript
 open WebSharper.JQuery
 open WebSharper.UI.Next
 open WebSharper.UI.Next.Client
+open WebSharper.UI.Next.Templating
 
 [<JavaScript>]
 module Client =
-    type IndexTemplate = Templating.Template<"index.html">
+    // The templates are loaded from the DOM, so you just can edit index.html
+    // and refresh your browser, no need to recompile unless you add or remove holes.
+    type IndexTemplate = Template<"index.html", ClientLoad.FromDocument>
 
     let People =
         ListModel.FromSeq [
@@ -16,21 +19,21 @@ module Client =
             "Paul"
         ]
 
-    let Main =
-        JQuery.Of("#main").Empty().Ignore
 
+    [<SPAEntryPoint>]
+    let Main () =
         let newName = Var.Create ""
 
-        IndexTemplate.Main.Doc(
-            ListContainer = [
-                People.View.DocSeqCached(fun name ->
-                    IndexTemplate.ListItem.Doc(Name = View.Const name)
+        IndexTemplate.Main()
+            .ListContainer(
+                People.View.DocSeqCached(fun (name: string) ->
+                    IndexTemplate.ListItem().Name(name).Doc()
                 )
-            ],
-            Name = newName,
-            Add = (fun el ev ->
+            )
+            .Name(newName)
+            .Add(fun _ ->
                 People.Add(newName.Value)
                 newName.Value <- ""
             )
-        )
+            .Doc()
         |> Doc.RunById "main"
